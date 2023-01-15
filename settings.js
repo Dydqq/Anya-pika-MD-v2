28190b72beb5ca24fddd43b69724827e705fa272
Dydqq <122739928+Dydqq@users.noreply.github.com>
2023-01-15 21:37:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,14 +16,14 @@ actiontitle = '࿙⚜𝑨𝒏𝒚𝒂⚜࿚' // text who displays on the top of 
 global.headerText = "Anya by Pika" // text for header of menu //        
      
              //-------------👑Owner & vcard 👑----------\\             
-             global.ownernumber = ['918811074852']   // Owner number //
-             global.vcardowner = ['918811074852']   // vcard owner number //
-             global.ownername = "3.69_Pika"   // Owner name //
+             global.ownernumber = ['919098723453']   // Owner number //
+             global.vcardowner = ['919098723453']   // vcard owner number //
+             global.ownername = "Victimized xO"   // Owner name //
              global.ytname = "YT: ONYXPIKA"   // Yt chanel name //
              global.socialm = "𝗚𝗶𝘁𝗵𝘂𝗯 : Pika4o4"   // Github or insta name //
              global.location = "India, Assam, Dibrugarh"   // Location //
              global.owner = ['918811074852','917355622763','919673141317']
-             global.ownertag = '918811074852'  // Tag number //
+             global.ownertag = '9189098723453'  // Tag number //
              global.linkz = "https://wa.me/+918811074852"  // Theme url which will be displayed on whatsapp //
              global.premium = ['918811074852']  // premium numbers //             
              global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark //
